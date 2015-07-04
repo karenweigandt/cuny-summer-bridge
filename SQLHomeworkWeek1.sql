f21e8616d0b25f60316a02663329d6426720745d
karenweigandt <karenw65e@gmail.com>
2015-07-04 13:26:37 -0400

    SQLHomeworkWeek1
    SQL file

---
+++ SQLHomeworkWeek1.sql
@@ -0,0 +1,10 @@
+﻿SELECT COUNT(*) FROM flights;
+SELECT carrier, COUNT(*) FROM flights GROUP BY carrier;
+SELECT carrier, COUNT(*) FROM flights GROUP BY carrier ORDER BY Count DESC;
+SELECT carrier, COUNT(*) FROM flights GROUP BY carrier ORDER BY Count DESC LIMIT 5;
+SELECT carrier, COUNT(*) FROM flights WHERE distance >= 1000 GROUP BY carrier ORDER BY Count DESC LIMIT 5;
+/*  
+Show top 10 airlines with the best average for on time arrivals
+ordered from least to most average delay 
+*/
+SELECT carrier, AVG(arr_delay) FROM flights GROUP BY carrier ORDER BY AVG(arr_delay) LIMIT 10;
